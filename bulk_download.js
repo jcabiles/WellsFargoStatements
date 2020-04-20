56f25ea781c3e3c75e5de1a154b0c1c840645b22
John Cabiles <johncabiles@gmail.com>
2020-04-20 15:36:29 -0700

    Add documentation to clarify steps

--- bulk_download.js
+++ bulk_download.js
@@ -1,3 +1,20 @@
+/**
+ * This script is based on the following thread: https://gist.github.com/binary1230/7cfa0524d0fae7c320e3b15fc1f4f64c
+ *
+ * To use it, follow these steps:
+ * 1) In Chrome browser, navigate to your Wells Fargo Statement Viewer and pull up the set of PDFs you want to view
+ * 2) Press F12 to open the developer tools and click the tab at the bottom that says 'Console'
+ * 3) In the developer console, paste the code that is shown under "Create the functions" section.  Then press ENTER.
+ * 4) Select the calendar year for which you want to download statements
+ * 5) Type this command in the developer console: openAll(lastResponse);
+ * 6) Press ENTER.  You'll know you were successful when a bunch of browser tabs open up.
+ *      Note: make sure that AdBlock allows you to open these tabs
+ * 7) Download the PDF files.  It might make later steps easier if you follow this naming convention: "yyyy-mm.pdf" 
+ * 8) Repeat steps 4-6 as needed
+ *
+ */
+ 
+
 /* Create functions to facilitate bulk downloads */
 function parseWFData(str) {
   str = str.substr(24);
@@ -32,3 +49,6 @@ function openAll(payload) {
     var urls = extractWFUrls(payload);
     urls.forEach(function(url){ window.open(url); });
 };
+
+
+/* Make sure to run this script once you finish */
