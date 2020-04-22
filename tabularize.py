aa5a4a020609f64a879aa95dead3e53fe66faa8d
John Cabiles <johncabiles@gmail.com>
2020-04-21 23:55:52 -0700

    Add func to find/run statements in dir

--- tabularize.py
+++ tabularize.py
@@ -4,7 +4,7 @@ import numpy as np
 from copy import copy
 from glob import glob
 
-statement_file = './StatementFiles/2017-08.pdf'
+statement_file = './StatementFiles/2017-07.pdf'
 rows = read_pdf(statement_file,
                 pages='all',
                 silent=True,
@@ -47,14 +47,22 @@ colnames = ['Date', 'Check Number', 'Description',
 july_dfs = [standardize_cols(row, colnames) for row in rows]
 july_dfs = pd.concat(july_dfs)
 
-# iterate over directory to find and process all PDF files
+
+# iterate over directory to find and pr
+# ocess all PDF files
+def loop_statement_files(statement_files_dir):
+    statements_dir = statement_files_dir
+    statement_files = glob(f'./{statement_files_dir}/*.pdf')
+    all_statements = []
+    for file in statement_files:
+        df_list = read_pdf(statement_file,
+                           pages='all',
+                           silent=True,
+                           encoding='utf-8',
+                           pandas_options={'header': None})
+        all_statements.append(df_list)
+    return all_statements
+
+
 statements_dir = './StatementFiles/'
-statement_files = glob('./StatementFiles/*.pdf')
-all_statements = []
-for file in statement_files:
-    rows = read_pdf(statement_file,
-                    pages='all',
-                    silent=True,
-                    encoding='utf-8',
-                    pandas_options={'header': None})
-    all_statements.append(rows)
+list_dfs = loop_statement_files(statements_dir)
