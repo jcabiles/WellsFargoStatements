74d33ab18fe1a655aa649198d98197ba8ed73492
John Cabiles <johncabiles@gmail.com>
2020-04-21 17:11:34 -0700

    Read in first statement as list of DFs

--- tabularize.py
+++ tabularize.py
@@ -1,3 +1,11 @@
-import pyPdf
-import tabula
+from tabula import read_pdf
+import pandas as pd
+import numpy as np
+from copy import copy
 
+statement_file = './StatementFiles/2017-07.pdf'
+rows = read_pdf(statement_file,
+                pages='all',
+                silent=True,
+                encoding='utf-8',
+                pandas_options={'header': None})
